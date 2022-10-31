86c89c1372f85995124208a88b64999abe4a1046
Eivind <ehaugw@gmail.com>
2022-10-31 17:17:48 +0100

    make tackle work mechanically

---
+++ Effects/InitTackle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Juggernaut
+{
+    class InitTackle : Effect
+    {
+        protected override void ActivateLocally(Character _affectedCharacter, object[] _infos)
+        {
+            var skill = _affectedCharacter.LastUsedSkill as MeleeSkill; 
+            //skill.MeleeHitDetector.LinecastTrans = _affectedCharacter.transform.FindAllInAllChildren("Clavicle_L").First();
+            skill.MeleeHitDetector.LinecastTrans = _affectedCharacter.transform.FindAllInAllChildren("hand_left").First();
+        }
+    }
+}
--- Spells/TackleSpell.cs
+++ Spells/TackleSpell.cs
@@ -13,7 +13,7 @@ namespace Juggernaut
             var myitem = new SL_AttackSkill()
             {
                 Name = "Tackle",
-                EffectBehaviour = EditBehaviours.Override,
+                EffectBehaviour = EditBehaviours.Destroy,
                 Target_ItemID = IDs.daggerSlashID,
                 New_ItemID = IDs.tackleID,
                 SLPackName = "Juggernaut",
@@ -25,7 +25,6 @@ namespace Juggernaut
                 MobileCastMovementMult = -1,
                 CastSheatheRequired = 0,
                 //RequiredWeaponTypes = new List<Weapon.WeaponType>() { Weapon.WeaponType.Axe_1H, Weapon.WeaponType.Axe_2H, Weapon.WeaponType.Sword_1H, Weapon.WeaponType.Sword_2H, Weapon.WeaponType.Mace_1H, Weapon.WeaponType.Mace_2H, Weapon.WeaponType.Halberd_2H, Weapon.WeaponType.Spear_2H },
-                
                 RequiredOffHandTypes = new Weapon.WeaponType[] { },
                 Cooldown = 30,
                 StaminaCost = 8,
@@ -33,54 +32,46 @@ namespace Juggernaut
                 HealthCost = 0,
             };
             myitem.ApplyTemplate();
-            Skill skill = ResourcesPrefabManager.Instance.GetItemPrefab(myitem.New_ItemID) as Skill;
+            MeleeSkill skill = ResourcesPrefabManager.Instance.GetItemPrefab(myitem.New_ItemID) as MeleeSkill;
+            skill.Blockable = false;
 
+            var meleeHitDetectorTransform = TinyGameObjectManager.GetOrMake(skill.transform, "MeleeHitDetector", true, true).gameObject;
+            skill.MeleeHitDetector = meleeHitDetectorTransform.gameObject.AddComponent<MeleeHitDetector>();
+            skill.MeleeHitDetector.Radius = 0.3f;
+            skill.MeleeHitDetector.LinecastCount = 3;
+            skill.MeleeHitDetector.Damage = -1;
             EmptyOffHandCondition.AddToSkill(skill, false, false);
-
-
-
+            
+            var activationEffects = TinyGameObjectManager.GetOrMake(skill.transform, "ActivationEffects", true, true).gameObject;
+            activationEffects.AddComponent<InitTackle>();
 
             //-------- ACTIVATION ----------//
-
-            var activationEffects = skill.transform.Find("ActivationEffects").gameObject;
             for (int i = 0; i < 4; i++)
             {
                 var hitenabler = activationEffects.AddComponent<EnableHitDetection>();
                 hitenabler.Delay = 0.2f + i * 0.08f;
             }
-            //var detector = skill.gameObject.GetComponentInChildren<MeleeHitDetector>();
-            //detector.Radius = 1f;
-
-            ////Sounds
-            //var activationEffects = skill.gameObject.transform.FindInAllChildren("ActivationEffects").gameObject;
-            foreach (var sound in activationEffects.GetComponentsInChildren<PlaySoundEffect>())
-            {
-                UnityEngine.Object.Destroy(sound);
-            }
-            var shieldCharge = ResourcesPrefabManager.Instance.GetItemPrefab(IDs.shieldChargeID).gameObject;
-            foreach (var sound in shieldCharge.gameObject.GetComponentsInChildren<PlaySoundEffect>())
-            {
-                if (sound.Delay <= 0.2)
-                {
-                    var newSound = activationEffects.AddComponent<PlaySoundEffect>();
-                    newSound.Sounds = sound.Sounds;
-                    newSound.Delay = sound.Delay;
-                    newSound.Follow = sound.Follow;
-                    newSound.MinPitch = sound.MinPitch;
-                    newSound.MaxPitch = sound.MaxPitch;
-                    newSound.SyncType = sound.SyncType;
-                }
-            }
+            
+            //var shieldCharge = ResourcesPrefabManager.Instance.GetItemPrefab(IDs.shieldChargeID).gameObject;
+            //foreach (var sound in shieldCharge.gameObject.GetComponentsInChildren<PlaySoundEffect>())
+            //{
+            //    if (sound.Delay <= 0.2)
+            //    {
+            //        var newSound = activationEffects.AddComponent<PlaySoundEffect>();
+            //        newSound.Sounds = sound.Sounds;
+            //        newSound.Delay = sound.Delay;
+            //        newSound.Follow = sound.Follow;
+            //        newSound.MinPitch = sound.MinPitch;
+            //        newSound.MaxPitch = sound.MaxPitch;
+            //        newSound.SyncType = sound.SyncType;
+            //    }
+            //}
 
             //-------- HIT ---------//
-            var hitEffects = skill.gameObject.transform.FindInAllChildren("HitEffects").gameObject;
+            var hitEffects = TinyGameObjectManager.GetOrMake(skill.transform, "HitEffects", true, true).gameObject;
             var tackleEffect = hitEffects.AddComponent<TackleEffect>();
             tackleEffect.Knockback = 10;
             tackleEffect.Damages = new DamageType[] { new DamageType(DamageType.Types.Physical, 3) };
-
-            var pickPunctualDamage = hitEffects.GetComponent<PunctualDamage>();
-            UnityEngine.Object.Destroy(pickPunctualDamage);
-
 
             return skill;
         }
